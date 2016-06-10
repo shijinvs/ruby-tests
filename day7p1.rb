8fcc55daef7dfafb0c1f87f4bdb738f520158eb2
Shijin Suresh V <shijinvs@gmail.com>
2016-06-10 17:27:31 +0530

    day 7 type conversion error

--- day7p1.rb
+++ day7p1.rb
@@ -1,24 +1,35 @@
-def day7p1(input)
-
-#NOT = lambda { |i| ~i }
-#AND = lambda { |i, j| i & j }
-#OR = lambda { |i, j| i | j }
-#LSHIFT = lambda {|i| i=i<<1}
-#RSHIFT = lambda {|i| i=i>>1}
-
-input.split("\n").each do |line|
-
-	line.match(/(^[a-z]\w*)/)[0] ? op1=line.match(/(^[a-z]\w*)/)[0] : op1=0 
-	operation=line.match(/([A-Z]\w*)/)[0]
-	op2=line.match(/(\s([a-z][a-z])\s\W)/)[0]
-	op3=line.match(/([a-z]\w$)/)[0]
-
-	puts op1,op2,op3,operation
+def day7p1
+value=Hash.new
+DATA.each_line do |line|
+	words = line.scan(/\S+/)
+	puts [words[0]].to_i
+	value[words[1]] = words[1].to_i if words[1] =~ /[(\d*)]/
+	value[words[2]] = words[1].to_i if words[2] =~ /[(\d*)]/
+	if words[1]=="->"
+		value[words[2]]=value[words[0]]
+	elsif words[2]=="->"
+		~value[words[1]]=value[words[3]]
+	elsif words[1]=="AND"
+		value[words[4]] = value[words[0]] & value[words[2]]
+	elsif words[1]=="OR"
+		value[words[4]] = value[words[0]] | value[words[2]]
+	elsif words[1]=="LSHIFT"
+		value[words[4]] = value[words[0]] << value[words[2]] 
+	else
+		value[words[4]] = value[words[0]] >> value[words[2]] 
+	end
 end
 end
 
+day7p1
 
-day7p1('af AND ah -> ai
-NOT lk -> ll
-hz RSHIFT 1 -> is
-NOT go -> gp')
+
+__END__
+123 -> x
+456 -> y
+x AND y -> d
+x OR y -> e
+x LSHIFT 2 -> f
+y RSHIFT 2 -> g
+NOT x -> h
+NOT y -> i
